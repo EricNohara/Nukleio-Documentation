b874ab5c66b9b6c220e5af95bcfda0365cab3fdc
Eric Nohara-LeClair <Eric.NoharaLeClair@dotfoods.com>
2026-07-08 15:50:21 -0500

    init

---
+++ .next/server/_rsc_content_getting-started_mdx_metadata-_rsc_content_index_mdx_metadata.js
@@ -0,0 +1,35 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_content_getting-started_mdx_metadata-_rsc_content_index_mdx_metadata";
+exports.ids = ["_rsc_content_getting-started_mdx_metadata-_rsc_content_index_mdx_metadata"];
+exports.modules = {
+
+/***/ "(rsc)/./content/getting-started.mdx?metadata":
+/*!**********************************************!*\
+  !*** ./content/getting-started.mdx?metadata ***!
+  \**********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   metadata: () => (/* binding */ metadata)\n/* harmony export */ });\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ \"(rsc)/./node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-runtime.js\");\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nconst metadata = {\n    \"title\": \"Getting Started\",\n    \"filePath\": \"content/getting-started.mdx\",\n    \"timestamp\": 1783543215265\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9jb250ZW50L2dldHRpbmctc3RhcnRlZC5tZHg/bWV0YWRhdGEiLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQXFFO0FBQzlELE1BQU1JLFdBQVc7SUFDdEIsU0FBUztJQUNULFlBQVk7SUFDWixhQUFhO0FBQ2YsRUFBRSIsInNvdXJjZXMiOlsiQzpcXFVzZXJzXFxFcmljLk5vaGFyYUxlQ2xhaXJcXE9uZURyaXZlIC0gRG90IEZvb2RzLCBJbmNcXERlc2t0b3BcXHBlcnNvbmFsXFxyZXBvc1xcTnVrbGVpby1Eb2N1bWVudGF0aW9uXFxjb250ZW50XFxnZXR0aW5nLXN0YXJ0ZWQubWR4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7RnJhZ21lbnQgYXMgX0ZyYWdtZW50LCBqc3ggYXMgX2pzeH0gZnJvbSBcInJlYWN0L2pzeC1ydW50aW1lXCI7XG5leHBvcnQgY29uc3QgbWV0YWRhdGEgPSB7XG4gIFwidGl0bGVcIjogXCJHZXR0aW5nIFN0YXJ0ZWRcIixcbiAgXCJmaWxlUGF0aFwiOiBcImNvbnRlbnQvZ2V0dGluZy1zdGFydGVkLm1keFwiLFxuICBcInRpbWVzdGFtcFwiOiAxNzgzNTQzMjE1MjY1XG59O1xuIl0sIm5hbWVzIjpbIkZyYWdtZW50IiwiX0ZyYWdtZW50IiwianN4IiwiX2pzeCIsIm1ldGFkYXRhIl0sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(rsc)/./content/getting-started.mdx?metadata\n");
+
+/***/ }),
+
+/***/ "(rsc)/./content/index.mdx?metadata":
+/*!************************************!*\
+  !*** ./content/index.mdx?metadata ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   metadata: () => (/* binding */ metadata)\n/* harmony export */ });\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ \"(rsc)/./node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-runtime.js\");\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nconst metadata = {\n    \"title\": \"Welcome to My Docs\",\n    \"filePath\": \"content/index.mdx\",\n    \"timestamp\": 1783543215265\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9jb250ZW50L2luZGV4Lm1keD9tZXRhZGF0YSIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBcUU7QUFDOUQsTUFBTUksV0FBVztJQUN0QixTQUFTO0lBQ1QsWUFBWTtJQUNaLGFBQWE7QUFDZixFQUFFIiwic291cmNlcyI6WyJDOlxcVXNlcnNcXEVyaWMuTm9oYXJhTGVDbGFpclxcT25lRHJpdmUgLSBEb3QgRm9vZHMsIEluY1xcRGVza3RvcFxccGVyc29uYWxcXHJlcG9zXFxOdWtsZWlvLURvY3VtZW50YXRpb25cXGNvbnRlbnRcXGluZGV4Lm1keCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQge0ZyYWdtZW50IGFzIF9GcmFnbWVudCwganN4IGFzIF9qc3h9IGZyb20gXCJyZWFjdC9qc3gtcnVudGltZVwiO1xuZXhwb3J0IGNvbnN0IG1ldGFkYXRhID0ge1xuICBcInRpdGxlXCI6IFwiV2VsY29tZSB0byBNeSBEb2NzXCIsXG4gIFwiZmlsZVBhdGhcIjogXCJjb250ZW50L2luZGV4Lm1keFwiLFxuICBcInRpbWVzdGFtcFwiOiAxNzgzNTQzMjE1MjY1XG59O1xuIl0sIm5hbWVzIjpbIkZyYWdtZW50IiwiX0ZyYWdtZW50IiwianN4IiwiX2pzeCIsIm1ldGFkYXRhIl0sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(rsc)/./content/index.mdx?metadata\n");
+
+/***/ })
+
+};
+;
